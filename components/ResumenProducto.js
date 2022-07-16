489da0d18203ac6359393755ce33dd6a68f16c70
cesar londoño <slac_@hotmail.com>
2022-07-16 00:17:37 -0400

    finished project 0003

--- components/ResumenProducto.js
+++ components/ResumenProducto.js
@@ -18,7 +18,7 @@ export const ResumenProducto = ({producto}) => {
           <p className='text-3xl font-bold'>{producto.nombre}</p>
           <p className='text-xl font-bold mt-2'>Cantidad: {producto.cantidad}</p>
           <p className='text-xl font-bold mt-2 text-amber-500'>
-            Precio: {formatearDinero(producto. precio)}
+            Precio: {formatearDinero(producto.precio)}
           </p>
           <p className='text-sm text-gray-700 mt-2'>
             Subtotal: {formatearDinero(producto.precio * producto.cantidad)}
