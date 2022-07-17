0c3fda9e4bb78a2d0a583417de8a135b5242c7b7
cesar londoño <slac_@hotmail.com>
2022-07-17 18:07:47 -0400

    finished project 0005

--- pages/api/ordenes.js
+++ pages/api/ordenes.js
@@ -1,7 +1,8 @@
 import {PrismaClient} from '@prisma/client'
 
 export default async function handler(req, res) {
-    const prisma = new PrismaClient
+
+    const prisma = new PrismaClient()
 
     if(req.method === "POST"){
         const orden = await prisma.orden.create({
@@ -10,8 +11,10 @@ export default async function handler(req, res) {
                 total: req.body.total,
                 pedido: req.body.pedido,
                 fecha: req.body.fecha,
-            }
+            },
         })
+        console.log(orden)
         res.json(orden)
     }
+
 }
